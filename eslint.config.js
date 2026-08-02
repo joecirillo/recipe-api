c9a6330924bb07d07bac851cdba8d706bd8d6f5d
Joe <48731689+joecirillo@users.noreply.github.com>
2026-08-01 18:28:44 -0700

    Adding eslint and prettier

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import prettierConfig from 'eslint-config-prettier';
+
+export default [
+  {
+    ignores: ['node_modules/**', 'dist/**'],
+  },
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+    },
+  },
+  prettierConfig,
+];
